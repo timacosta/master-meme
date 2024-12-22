9d13459bb27497a344db139ac6aff5a78629fccb
timacosta <timacosta06@gmail.com>
2024-12-22 14:32:47 +0000

    feat: fix windows insets and bottom sheet errors

--- app/src/main/kotlin/com/acostim/mastermeme/MainActivity.kt
+++ app/src/main/kotlin/com/acostim/mastermeme/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.acostim.mastermeme
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.acostim.mastermeme.ui.theme.ChineseBlack
 import com.acostim.mastermeme.ui.theme.MastermemeTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.auto(ChineseBlack.toArgb(), ChineseBlack.toArgb())
+        )
         setContent {
             MastermemeTheme {
                 MasterMemeApp(modifier = Modifier.fillMaxSize())
